686d894ef24410bf6cb4b863ce36b59e2fa4e5f8
melf <mel.f@bestgame-co.com>
2024-02-23 13:48:44 +0800

    test

--- js/main.js
+++ js/main.js
@@ -306,6 +306,43 @@ class ThemeSwitcher {
     }
 }
 
+class GoogleAppsScript {
+	constructor() {
+		this.GoogleAppsScriptId = "AKfycbw2UpKVFFIcDEKxEpTd_nhcMhxg5cdcHYziNawwO6NmwlnDrwbpH0E_MwCpQexi3Rk-dg"
+		this.GoogleSheetId = "1PmSGaEcacXDKymvkgW3oqEdRyIG2OJ8KyWVbXZXhuqI"
+
+	}
+	static get() {
+
+	}
+	static set(todo = {}) {
+		/* todo
+		id: this.getRandomId(),
+        task: task,
+        remark: remark,
+        dueDate: this.todoItemFormatter.formatDueDate(dueDate),
+        completed: false,
+        status: "pending",
+        * */
+		let name = document.querySelector('#nameValue').value;
+		let age = document.querySelector('#ageValue').value;
+		todo.SpreadsheetId = this.GoogleSheetId
+		console.info(todo)
+		$.ajax({
+			data: todo,
+			url: `https://script.google.com/macros/s/${this.GoogleAppsScriptId}/exec`,
+			success: function(response) {
+				if(response == "存入成功"){
+					alert("存入成功");
+				}
+			},
+		});
+	}
+	remove() {
+
+	}
+}
+
 
 
 // Instantiating the classes
@@ -315,3 +352,4 @@ const uiManager = new UIManager(todoManager, todoItemFormatter);
 const themes = document.querySelectorAll(".theme-item");
 const html = document.querySelector("html");
 const themeSwitcher = new ThemeSwitcher(themes, html);
+const gs = new GoogleAppsScript();
